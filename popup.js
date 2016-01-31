c575a951728883e63b1f314f385478f6f54baf9d
f00sek <biale.jerozy@gazeta.pl>
2016-01-31 19:37:00 +0100

    Fetching projects and filling select.

--- popup.js
+++ popup.js
@@ -1,13 +1,3 @@
-var token = "";
-
-document.addEventListener('DOMContentLoaded', function() {
-  var opener = document.getElementById('opener');
-  
-  opener.addEventListener('click', function() {
-    console.log("obtained: " + token);
-  });
-});
-
 var todoistOauth = new OAuth2('todoist', {
   'client_id': "21b13def0a134167bfd1695ad87c134f",
   "client_secret": "d5cf8b4ea94c46f18d25e7bf1e80b901",
@@ -16,5 +6,39 @@ var todoistOauth = new OAuth2('todoist', {
 });
 
 todoistOauth.authorize(function() {
-  token = todoistOauth.getAccessToken();
+  console.log("authorized: " + todoistOauth.getAccessToken());
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchProjects();
+});
+
+function fetchProjects() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(event) {
+    if (xhr.readyState == 4) {
+      if(xhr.status == 200) {
+        var response = JSON.parse(xhr.responseText);
+        var projects = response.Projects;
+        
+        var projectsSelect = document.getElementById('projects');
+        for(var i = 0; i < projects.length; i++) {
+          var option = document.createElement('option');
+          option.text = projects[i].name;
+          option.value = projects[i].id;
+          projectsSelect.add(option, null);
+        }
+      }
+    }
+  };
+  
+  var message = "token=" + todoistOauth.getAccessToken()
+    + "&seq_no=0"
+    + "&resource_types=[\"projects\"]";
+  
+  console.log(message);
+  
+  xhr.open('POST', 'https://todoist.com/API/v6/sync', true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send(message);
+}
